9ff2cc84eb66881563d987f671eaff6d84d83001
ak1103dev <ak.apichan1103@gmail.com>
2016-03-04 18:36:44 +0700

    week06-2

---
+++ lab06/non-restoring/stim.h
@@ -0,0 +1,27 @@
+#include <systemc.h>
+#include "bit.h"
+
+SC_MODULE(stim) {
+    sc_in<bool> clk;
+    sc_out<sc_uint<N> > RD, RV;
+
+	void test(unsigned int a, unsigned int b)
+	{
+		RD.write(a);
+		RV.write(b);
+		wait();
+	}
+
+
+    void ps1() {
+		test(0, 0);
+		test(30, 4);
+		test(30, 6);
+		sc_stop();
+    }
+
+    SC_CTOR(stim) {
+        SC_THREAD(ps1);
+        sensitive << clk.pos();
+    }
+};
